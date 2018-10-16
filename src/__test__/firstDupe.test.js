d8f099eecf3633f5ab8cfb681c22d3f983fafcbd
diego-ramos130 <diego.onethreezero@gmail.com>
2018-10-16 16:15:59 -0700

    tests done with

--- src/__test__/firstDupe.test.js
+++ src/__test__/firstDupe.test.js
@@ -1,9 +1,21 @@
 'use strict';
 
-import firstDupe from '../lib/firstDupe';
+const firstDupe = require('../lib/firstDupe');
 
 describe('firstdupe.js', () => {
   test('happy path: input has duplicate words in it and spaces.', () => {
     expect(firstDupe('cat dog cat dog')).toEqual('cat');
-  })
+  });
+  test('unhappy path: no spaces.', () => {
+    expect(firstDupe('catdogcatdog')).toEqual(undefined);
+  });
+  test('unhappy path: no dupes.', () => {
+    expect(firstDupe('cat dog catdog')).toEqual(undefined);
+  });
+  test('unhappy path: not above 2 words', () => {
+    expect(firstDupe('cat dog')).toEqual(undefined);
+  });
+  test('unhappy path: a lot of spaces', () => {
+    expect(firstDupe('           ')).toEqual('');
+  });
 });
